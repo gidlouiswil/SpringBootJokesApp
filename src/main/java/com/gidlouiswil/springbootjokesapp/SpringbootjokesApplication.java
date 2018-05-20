b061257dc8f17cc101ce2f196169300e1b334dc3
Louis Wilfried MBOG <louis.mbog.1@ens.etsmtl.ca>
2018-05-19 20:43:06 -0400

    The first commit

---
+++ src/main/java/com/gidlouiswil/springbootjokesapp/SpringbootjokesApplication.java
@@ -0,0 +1,13 @@
+package com.gidlouiswil.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjokesApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjokesApplication.class, args);
+	}
+}
